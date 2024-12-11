4f37a62f568cf75b4a4f07a4362fe327eb8cbcb3
meetmakwana7396 <makwnameet7301@gmail.com>
2024-12-11 23:58:13 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
+import Header from "@/components/layout/header";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div>
+      <main>
+        <Header />
+        <section className="py-32 container flex items-center">
+          <div className="flex flex-col lg:flex-row w-full justify-between items-center">
+            <div className="mb-8 max-w-4xl">
+              <p className="text-md tracking-widest mb-4 font-semibold uppercase text-blue-600">
+                On-demand taxis with just a call away.
+              </p>
+              <h1 className="text-8xl font-semibold text-gray-800 mb-4">
+                Reliable Taxi Service, Anytime
+              </h1>
+              <p className="text-xl mb-8 font-semibold text-gray-600">
+                Convenient, fast, and dependable rides.
+              </p>
+              <Button
+                size={"lg"}
+                className="bg-yellow-400  text-lg text-black hover:text-white font-semibold px-8 h-14 rounded-lg transition duration-300"
+              >
+                Book Your Ride
+              </Button>
+            </div>
+            <div className="relative w-full h-96 lg:h-auto">
+              <Image
+                src="/images/hero-img.jpg"
+                alt="Hero-img"
+                layout="responsive"
+                width={800}
+                height={600}
+                className="object-cover rounded-lg"
+              />
+            </div>
+          </div>
+        </section>
+        {/* <div className="container flex py-20 justify-between items-center">
+          <div className="space-y-6">
+            <span className="tracking-widest text-xl font-semibold text-blue-500 uppercase">
+              10+ Years and Still now
+            </span>
+            <h1 className="text-8xl font-extrabold max-w-2xl upp">
+              Reliable Taxi Service, Anytime
+            </h1>
+          </div>
+          <Image
+            src="/images/hero-img.jpg"
+            height={400}
+            width={800}
+            className="h-full max-w-[800x]"
+            alt="Hero-img"
+          />
+        </div> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
